cba21e588b909c6654f1e652a82e943494812765
Maxim Kernozhitskiy <ctajikep@gmail.com>
2018-05-07 10:20:54 +0300

    [feat] Move components to wrapper and filter state

--- src/App.js
+++ src/App.js
@@ -7,6 +7,48 @@ import styles from './App.css'
 import { state$ } from './store'
 import connect from './connect'
 
+const filterEnv$ = state$.map(s => s.filterEnv).skipDuplicates()
+const ampEnv$ = state$.map(s => s.ampEnv).skipDuplicates()
+const mixer$ = state$.map(s => s.mixer).skipDuplicates()
+
+function FilterEnv() {
+  const FilterEnvelope = connect(
+    { filterEnv: filterEnv$ },
+    ({ filterEnv }) => {
+      return (
+        <Envelope name='Filter' path='filterEnv' debug={true} data={filterEnv} />
+      )
+    }
+  )
+
+  return <FilterEnvelope />
+}
+
+function AmpEnv() {
+  const AmpEnvelope = connect(
+    { ampEnv: ampEnv$ },
+    ({ ampEnv }) => {
+      return (
+        <Envelope name='Amp' path='ampEnv' data={ampEnv} />
+      )
+    }
+  )
+
+  return <AmpEnvelope />
+}
+
+function OscMixer() {
+  const OscillatorMixer = connect(
+    { mixer: mixer$ },
+    ({ mixer }) => {
+      return (
+        <Mixer name='Mixer' data={mixer} />
+      )
+    }
+  )
+
+  return <OscillatorMixer />
+}
 
 function App() {
   const Synth = connect(
@@ -19,20 +61,9 @@ function App() {
             <h1>{state.keyboard.note}</h1>
           </header>
           <Keyboard />
-          <Envelope
-            name='Filter'
-            path='filterEnv'
-            data={state.filterEnv}
-          />
-          <Envelope
-            name='Amp'
-            path='ampEnv'
-            data={state.ampEnv}
-          />
-          <Mixer
-            name='Mixer'
-            data={state.mixer}
-          />
+          <FilterEnv />
+          <AmpEnv />
+          <OscMixer />
         </div>
       )
     }
